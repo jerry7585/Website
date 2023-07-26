b9ce5704fe5401541711c0db351f84151804e50e
Jerry Su <jerrysu.nyc@gmail.com>
2023-07-25 22:56:45 -0400

    Added bootstrap so the menu bar will change colors once scrolled and expanded on About me Section

--- js/script.js
+++ js/script.js
@@ -1,17 +1,16 @@
-var navbar = document.getElementById('menubar');
-var bar = document.querySelector('.bar');
-var navbarLinks = document.querySelectorAll('.navbar-nav li a');
+document.addEventListener('DOMContentLoaded', function() {
+  var navbarLinks = document.querySelectorAll('.navbar-nav li a');
+  var bar = document.querySelector('.bar');
 
-window.addEventListener('scroll', function() {
-  if (window.scrollY > 0) {
-    navbar.classList.add('scrolled');
-    bar.classList.add('scrolled-bar');
-    navbarLinks.forEach(link => link.classList.add('scrolled-navbar-nav'));
-    console.log('JavaScript is working!');
-  } else {
-    navbar.classList.remove('scrolled');
-    bar.classList.remove('scrolled-bar');
-    navbarLinks.forEach(link => link.classList.remove('scrolled-navbar-nav'));
-    console.log('JavaScript is NOT working!');
-  }
+  window.addEventListener('scroll', function() {
+    if (window.scrollY > 0) {
+      navbarLinks.forEach(link => link.classList.add('scrolled-navbar-nav'));
+      bar.classList.add('scrolled-bar');
+      console.log('JavaScript is working!');
+    } else {
+      navbarLinks.forEach(link => link.classList.remove('scrolled-navbar-nav'));
+      bar.classList.remove('scrolled-bar');
+      console.log('JavaScript is NOT working!');
+    }
+  });
 });
