421ef5ce33edb99ec0ecbd039de079127a9759b5
Jerry Su <jerrysu.nyc@gmail.com>
2023-07-26 22:24:57 -0400

    Added more to about me section/polished up the smooth scrolling of learn more and navigation menu buttons/made menubar turn white after scrolling with shadow

--- js/script.js
+++ js/script.js
@@ -1,16 +1,66 @@
+/*
+    Code to make bar at top static and change colors
+*/
+
 document.addEventListener('DOMContentLoaded', function() {
   var navbarLinks = document.querySelectorAll('.navbar-nav li a');
   var bar = document.querySelector('.bar');
 
   window.addEventListener('scroll', function() {
     if (window.scrollY > 0) {
-      navbarLinks.forEach(link => link.classList.add('scrolled-navbar-nav'));
+      document.querySelector('.navbar-nav').classList.add('scrolled');
       bar.classList.add('scrolled-bar');
-      console.log('JavaScript is working!');
+      console.log('Navigation bar: JS working!');
     } else {
-      navbarLinks.forEach(link => link.classList.remove('scrolled-navbar-nav'));
+      document.querySelector('.navbar-nav').classList.remove('scrolled');
       bar.classList.remove('scrolled-bar');
-      console.log('JavaScript is NOT working!');
+      console.log('Navigation bar: JS NOT working!');
     }
+  });
+});
+
+
+/*
+    Code to allow for smooth scrolling on "learn more" button
+*/
+document.addEventListener('DOMContentLoaded', function() {
+  var learnMoreLink = document.getElementById('learn-more-btn');
+
+  learnMoreLink.addEventListener('click', function(event) {
+      event.preventDefault(); // Prevent the default anchor behavior
+
+      var targetElement = document.getElementById('about');
+      var targetOffset = targetElement.getBoundingClientRect().top;
+      var headerOffset = 60; // Adjust this value to account for your fixed header height
+
+      // Calculate the final scroll position
+      var scrollTarget = targetOffset + window.scrollY - headerOffset;
+
+      // Smooth scroll to the target element
+      window.scrollTo({
+          top: scrollTarget,
+          behavior: 'smooth'
+      });
+  });
+});
+
+/*
+    Code to allow for smooth scrolling on navigation menu items
+*/
+document.addEventListener('DOMContentLoaded', function() {
+  var navLinks = document.querySelectorAll('.smooth-scroll');
+
+  navLinks.forEach(link => {
+    link.addEventListener('click', function(event) {
+      event.preventDefault(); // Prevent default link behavior
+
+      var target = document.querySelector(link.getAttribute('href'));
+      var offset = 60; // Adjust this offset if you have a fixed navigation bar
+
+      if (target) {
+        var top = target.getBoundingClientRect().top + window.scrollY - offset;
+        window.scrollTo({ top: top, behavior: 'smooth' });
+      }
+    });
   });
 });
